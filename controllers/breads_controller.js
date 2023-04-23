9a2dfa91d18d1cfe952782ec88bbbe93aaaa1e9c
Josh Wedekind <josh@halfnibble.com>
2023-04-23 12:08:37 -0700

    Added bread datamodel.

--- controllers/breads_controller.js
+++ controllers/breads_controller.js
@@ -1,9 +1,17 @@
 const express = require("express");
 const breads = express.Router();
 
-// INDEX
+const Bread = require("../models/bread.js");
+
+// INDEX - READ ALL
 breads.get("/", (req, res) => {
-    res.send("This is the index at /breads");
+    res.send(Bread);
+});
+
+// READ ONE?
+breads.get("/:arrayIndex", (req, res) => {
+    const arrayIndex = req.params.arrayIndex;
+    res.send(Bread[arrayIndex]);
 });
 
 // EXPORT
