883bda215048e2df363cf9972e5a7b284331222b
Josh Wedekind <josh@halfnibble.com>
2023-04-25 21:25:31 -0700

    Add bread form

--- controllers/breads_controller.js
+++ controllers/breads_controller.js
@@ -11,6 +11,10 @@ breads.get("/", (req, res) => {
     });
 });
 
+breads.get("/new", (req, res) => {
+    res.render("new");
+});
+
 // READ ONE
 breads.get("/:arrayIndex", (req, res) => {
     const arrayIndex = req.params.arrayIndex;
@@ -21,6 +25,26 @@ breads.get("/:arrayIndex", (req, res) => {
     } else {
         res.send("404");
     }
+});
+
+// CREATE
+breads.post("/", (req, res) => {
+    let newBread = { ...req.body };
+    // Default bread image
+    if (newBread.image === "") {
+        newBread.image =
+            "https://images.unsplash.com/photo-1517686469429-8bdb88b9f907?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=80";
+    }
+    // Process hasGluten checkbox
+    if (newBread.hasGluten === "on") {
+        newBread.hasGluten = true;
+    } else if (newBread.hasGluten === "off") {
+        newBread.hasGluten = false;
+    } else {
+        console.error("Error: hasGluten value is:", newBread.hasGluten);
+    }
+    Bread.push(newBread);
+    res.redirect("/breads");
 });
 
 // EXPORT
