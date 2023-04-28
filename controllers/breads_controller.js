65849959a0bf8d6e13ff4b3a450d1dd923463b4e
Josh Wedekind <josh@halfnibble.com>
2023-04-27 21:22:18 -0700

    Edit and update routes complete.

--- controllers/breads_controller.js
+++ controllers/breads_controller.js
@@ -15,7 +15,16 @@ breads.get("/new", (req, res) => {
     res.render("new");
 });
 
-// READ ONE
+// EDIT
+breads.get("/:arrayIndex/edit", (req, res) => {
+    const arrayIndex = req.params.arrayIndex;
+    res.render("edit", {
+        bread: Bread[arrayIndex],
+        index: arrayIndex,
+    });
+});
+
+// READ ONE - SHOW
 breads.get("/:arrayIndex", (req, res) => {
     const arrayIndex = req.params.arrayIndex;
     if (Bread[arrayIndex]) {
@@ -46,6 +55,27 @@ breads.post("/", (req, res) => {
     }
     Bread.push(newBread);
     res.redirect("/breads");
+});
+
+// UPDATE
+breads.put("/:arrayIndex", (req, res) => {
+    const arrayIndex = req.params.arrayIndex;
+    let updatedBread = { ...req.body };
+    // Default bread image
+    if (updatedBread.image === "") {
+        updatedBread.image =
+            "https://images.unsplash.com/photo-1517686469429-8bdb88b9f907?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=80";
+    }
+    // Process hasGluten checkbox
+    if (updatedBread.hasGluten === "on") {
+        updatedBread.hasGluten = true;
+    } else if (updatedBread.hasGluten === "off") {
+        updatedBread.hasGluten = false;
+    } else {
+        console.error("Error: hasGluten value is:", updatedBread.hasGluten);
+    }
+    Bread[arrayIndex] = updatedBread;
+    res.redirect(`/breads/${arrayIndex}`);
 });
 
 // DELETE
--- views/show.jsx
+++ views/show.jsx
@@ -16,6 +16,9 @@ const Show = ({ bread, index }) => {
             <li>
                 <a href="/breads">Go home</a>
             </li>
+            <a href={`/breads/${index}/edit`}>
+                <button>Edit</button>
+            </a>
             <form action={`/breads/${index}?_method=DELETE`} method="POST">
                 <input type="submit" value="DELETE" />
             </form>
